d277d0fa9f2de8c19e92bb4847571681e902f381
Gilary <Gilary_FB@live.nl>
2019-01-24 13:55:25 +0100

    95.creating a component

---
+++ src/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>server status: {{ status }}</p>
+    <hr>
+    <button @click="changeStatus">verander status</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+        status: 'critical'
+    }
+  },
+  methods: {
+    changeStatus() {
+      this.status = 'Normal';
+    }
+
+  }
+}
+</script>
+
+<style>
+
+</style>
+
+
