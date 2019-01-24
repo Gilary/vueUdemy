4f21765209b4bd3c35165a38ad19d210df61b537
Gilary <Gilary_FB@live.nl>
2019-01-24 14:54:48 +0100

    opdracht 6: made different components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
-  <app-server></app-server>
+  <div class="container">  
+    <Titel />
+    <hr>
+    <Server /> 
+    <hr>
+    <Footer />
+  </div>
 </template>
 
 <script>
+  import Titel from './Titel.vue';
+  import Server from './Server.vue';
+  import Footer from './Footer.vue';
+  export default {
+    components:{
+      Titel,
+      Server,
+      Footer
+    }
+  }
+
 </script>
 
 <style>
+
 </style>
