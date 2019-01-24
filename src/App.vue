8306609598a6ecf045f02fcf465f35cdebba2180
Gilary <Gilary_FB@live.nl>
2019-01-24 15:23:08 +0100

    97.Moving to better Folder Structure

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,9 @@
 </template>
 
 <script>
-  import Titel from './Titel.vue';
-  import Server from './Server.vue';
-  import Footer from './Footer.vue';
+  import Titel from './components/Shared/Titel.vue';
+  import Server from './components/Server/Server.vue';
+  import Footer from './components/Shared/Footer.vue';
   export default {
     components:{
       Titel,
--- src/components/Server/Server.vue
+++ src/components/Server/Server.vue
@@ -2,11 +2,11 @@
 	<div class="row">
 		<div class="col-xs-12 col-sm-6">
 			<ul class="list-group">
-					<li
-						class="list-group-item"
-						v-for="index in 5">
-						Server #{{ index }}
-					</li>
+				<li
+					class="list-group-item"
+					v-for="index in 5">
+					Server #{{ index }}
+				</li>
 			</ul>
 		</div>
 		<div class="col-xs-12 col-sm-6">
