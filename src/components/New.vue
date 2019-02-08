eab003ba8e19f994331fe1024e416f98eb33c49a
Tilly Brito <gilary.brito@aon.nl>
2019-02-08 14:38:19 +0100

    127.Understanding dynamic component behavior

--- src/components/New.vue
+++ src/components/New.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
     <h3>Nieuw</h3>
+    <button @click="counter++">Meer!</button>
+    <p>{{counter}}</p>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data(){
+    return{
+      counter:0
+    };
+  },
+  destroyed(){
+    console.log('kapot!')
+  }
+};
 </script>
 
 <style scoped>
